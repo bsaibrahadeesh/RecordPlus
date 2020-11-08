03bf23a60a015acaeb28bb06c269c36e79f76eb1
SandyVH <sandhya101001@gmail.com>
2020-11-09 00:58:58 +0530

    Changed the login page with buttons and fragments

---
+++ app/src/main/java/com/example/healthproject/FirstFragment.java
@@ -0,0 +1,63 @@
+package com.example.healthproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FirstFragment extends Fragment implements View.OnClickListener {
+
+    View view;
+    Button login1;
+    EditText username1, pass1;
+    SQLiteDatabase db;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        username1 = view.findViewById(R.id.username1);
+        pass1 = view.findViewById(R.id.password1);
+        login1 = view.findViewById(R.id.login1);
+        login1.setOnClickListener(this);
+        db = getActivity().openOrCreateDatabase("StudentDB", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS Student(rollno VARCHAR,name VARCHAR,password VARCHAR,emailid VARCHAR,phonenumber VARCHAR,dateofbirth VARCHAR,parentnumber1 VARCHAR,parentnumber2 VARCHAR,hostel VARCHAR,roomno VARCHAR);");
+        return view;
+
+    }
+
+    @Override
+    public void onClick(View view) {
+            if (username1.getText().toString().length() == 0 || pass1.getText().toString().length() == 0) {
+                Toast.makeText(getActivity(), "Enter Rollno/password", Toast.LENGTH_SHORT).show();
+            }
+            Cursor c = db.rawQuery("SELECT * FROM Student WHERE rollno='" + username1.getText() + "' and password='" + pass1.getText() + "'", null);
+            if (c.moveToFirst()) {
+                username1.setText("");
+                pass1.setText("");
+                Intent i = new Intent(getActivity(), userbase.class);
+                i.putExtra("rollno", c.getString(0));
+                Toast.makeText(getActivity(), "Successfully Logged In", Toast.LENGTH_LONG).show();
+                startActivity(i);
+            } else {
+                Toast.makeText(getActivity(), "Invalid Rollno/password", Toast.LENGTH_SHORT).show();
+            }
+    }
+
+}
--- app/src/main/java/com/example/healthproject/Loginpage.java
+++ app/src/main/java/com/example/healthproject/Loginpage.java
@@ -1,47 +1,76 @@
 package com.example.healthproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.ViewCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class Loginpage extends AppCompatActivity implements View.OnClickListener {
-    EditText username1, pass1, username2, pass2;
-    Button login1, login2;
-    SQLiteDatabase db, db1;
+public class Loginpage extends AppCompatActivity {
+    Button firstFragment, secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
 
+        firstFragment =  findViewById(R.id.frag1);
+        secondFragment = findViewById(R.id.frag2);
+
         Toolbar toolbar =findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        username1 = findViewById(R.id.username1);
-        pass1 = findViewById(R.id.password1);
-        login1 = findViewById(R.id.login1);
-        username2 = findViewById(R.id.username2);
-        pass2 = findViewById(R.id.password2);
-        login2 = findViewById(R.id.login2);
-        login1.setOnClickListener(this);
-        login2.setOnClickListener(this);
-        db = openOrCreateDatabase("StudentDB", Context.MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS Student(rollno VARCHAR,name VARCHAR,password VARCHAR,emailid VARCHAR,phonenumber VARCHAR,dateofbirth VARCHAR,parentnumber1 VARCHAR,parentnumber2 VARCHAR,hostel VARCHAR,roomno VARCHAR);");
-        db1 = openOrCreateDatabase("AdminDB", Context.MODE_PRIVATE, null);
-        db1.execSQL("CREATE TABLE IF NOT EXISTS Admin(username VARCHAR,name VARCHAR,password VARCHAR);");
+        loadFragment(new FirstFragment());
+        firstFragment.setTextColor(Color.rgb(25, 140, 35));
+
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // load First Fragment
+                loadFragment(new FirstFragment());
+                firstFragment.setTextColor(Color.rgb(25, 140, 35));
+                secondFragment.setTextColor(Color.BLACK);
+            }
+        });
+
+        // perform setOnClickListener event on Second Button
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // load Second Fragment
+                loadFragment(new SecondFragment());
+                secondFragment.setTextColor(Color.rgb(25, 140, 35));
+                firstFragment.setTextColor(Color.BLACK);
+            }
+        });
+    }
+
+
+    private void loadFragment(Fragment fragment) {
+        // create a FragmentManager
+        FragmentManager fm = getSupportFragmentManager();
+
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction =
+                fm.beginTransaction();
+
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+
+        //fragmentTransaction.add(R.id.firstFragment,fragment);
+        fragmentTransaction.commit(); // save the changes
     }
 
     @Override
@@ -52,48 +81,5 @@ public class Loginpage extends AppCompatActivity implements View.OnClickListener
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view == login1) {
-            if (username1.getText().toString().length() == 0 || pass1.getText().toString().length() == 0) {
-                Toast.makeText(this, "Enter Rollno/password", Toast.LENGTH_SHORT).show();
-            }
-            Cursor c = db.rawQuery("SELECT * FROM Student WHERE rollno='" + username1.getText() + "' and password='" + pass1.getText() + "'", null);
-            if (c.moveToFirst()) {
-                username1.setText("");
-                pass1.setText("");
-                Intent i = new Intent(getApplicationContext(), userbase.class);
-                i.putExtra("rollno", c.getString(0));
-                Toast.makeText(getApplicationContext(), "Successfully Logged In", Toast.LENGTH_LONG).show();
-                startActivity(i);
-            } else {
-                Toast.makeText(this, "Invalid Rollno/password", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            try {
-                Cursor c = db1.rawQuery("SELECT * FROM Admin WHERE username='Admin'", null);
-                if (!(c.moveToFirst())) {
-                    db1.execSQL("INSERT INTO Admin VALUES('Admin','Admin','Admin');");
-                }
-            } catch (Exception e) {
-                Log.i("Error", e.getMessage().toString());
-            }
-            if (username2.getText().toString().length() == 0 || pass2.getText().toString().length() == 0) {
-                Toast.makeText(this, "Enter Username/password", Toast.LENGTH_SHORT).show();
-            }
-            Cursor c = db1.rawQuery("SELECT * FROM Admin WHERE username='" + username2.getText() + "' and password='" + pass2.getText() + "'", null);
-            if (c.moveToFirst()) {
-                username2.setText("");
-                pass2.setText("");
-                Intent i = new Intent(getApplicationContext(), adminbase.class);
-                i.putExtra("username", c.getString(0));
-                Toast.makeText(getApplicationContext(), "Successfully Logged In", Toast.LENGTH_LONG).show();
-                startActivity(i);
-            } else {
-                Toast.makeText(this, "Invalid Username/password", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
